8f0dbe4c52ee0268959c713f3f566d96934da1c0
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-09-07 01:40:18 -0400

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Nicholas McGinnis on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
