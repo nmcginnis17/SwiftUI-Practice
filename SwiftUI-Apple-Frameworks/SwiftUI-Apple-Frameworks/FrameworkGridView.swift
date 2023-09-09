f66687bdd870d84c06c8697c86b68434d3fb463c
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-09-09 00:17:22 -0400

    Frameworks gridLayout

---
+++ SwiftUI-Apple-Frameworks/SwiftUI-Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,56 @@
+//
+//  FrameworkGridView.swift
+//  SwiftUI-Apple-Frameworks
+//
+//  Created by Nicholas McGinnis on 9/8/23.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks, id: \.id) { framework in
+                        FrameworkTitleView(framework: framework)
+                    }
+                }
+            }
+            .navigationTitle("🖥️ Frameworks")
+        }
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+            .preferredColorScheme(.dark)
+    }
+}
+
+
+
+struct FrameworkTitleView: View {
+    
+    let framework: Framework
+    
+    var body: some View {
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+        .padding()
+    }
+}
