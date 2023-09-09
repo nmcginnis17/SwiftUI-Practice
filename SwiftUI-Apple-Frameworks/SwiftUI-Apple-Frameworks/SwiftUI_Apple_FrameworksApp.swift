a591b4de084a6da346aee67efef69353e8244f02
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-09-08 23:25:57 -0400

    Frameworks project initial commit

---
+++ SwiftUI-Apple-Frameworks/SwiftUI-Apple-Frameworks/SwiftUI_Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Apple_FrameworksApp.swift
+//  SwiftUI-Apple-Frameworks
+//
+//  Created by Nicholas McGinnis on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
