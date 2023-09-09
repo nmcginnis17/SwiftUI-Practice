53629cd3a6d9638887ae652e02707ec51c40dda9
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-09-09 08:31:02 -0400

    Start detailView

--- SwiftUI-Apple-Frameworks/SwiftUI-Apple-Frameworks/Framework.swift
+++ SwiftUI-Apple-Frameworks/SwiftUI-Apple-Frameworks/Framework.swift
@@ -16,7 +16,7 @@ struct Framework: Hashable, Identifiable {
 }
 
 struct MockData {
-    
+        
     static let sampleFramework = Framework(name: "ARKit",
                                            imageName: "arkit",
                                            urlString: "https://developer.apple.com/augmented-reality/arkit",
---
+++ SwiftUI-Apple-Frameworks/SwiftUI-Apple-Frameworks/FrameworkDetailView.swift
@@ -0,0 +1,42 @@
+//
+//  FrameworkDetailView.swift
+//  SwiftUI-Apple-Frameworks
+//
+//  Created by Nicholas McGinnis on 9/9/23.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    
+    var framework: Framework
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            FrameworkTitleView(framework: framework)
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            Spacer()
+            Button {
+                
+            } label: {
+                Text("Learn More")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .frame(width: 280, height: 50)
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
+    }
+}
+
+struct FrameworkDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkDetailView( framework: MockData.sampleFramework)
+    }
+}
+
