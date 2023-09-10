3e778e9d2263adfd433d65a0eb9496e5bb4c61a7
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-09-10 19:06:00 -0400

    Added Safari View for learn more

---
+++ SwiftUI-Apple-Frameworks/SwiftUI-Apple-Frameworks/SafariView.swift
@@ -0,0 +1,23 @@
+//
+//  SafariView.swift
+//  SwiftUI-Apple-Frameworks
+//
+//  Created by Nicholas McGinnis on 9/10/23.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+        //
+    }
+    
+}
